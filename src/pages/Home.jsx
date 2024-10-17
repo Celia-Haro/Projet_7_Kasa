f27ee208fef4b3e4e01e73bd50e65ee92a72a9ea
Celia Haro <charo@keldin.fr>
2024-10-17 13:00:04 +0200

    fix: path ErrorPage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import Banner from "../components/home/Banner"
+import logements from '../assets/data/logements.json'
 import { Link } from "react-router-dom"
 
+
+// const logements = [{ id: 1, name: 'Pomme', color: 'rouge' }, { id: 2, name: 'Banane', color: 'jaune' }, { id: 3, name: 'Orange', color: 'orange' }];
 export default function Home() {
     return (
         <>
             <Banner />
             <div>
                 {/* 1 card / Item ( logements.json) */}
-                <article> <Link to="/lodgingdetail">Je suis une carte logement</Link></article>
-                <article>Je suis une carte logement</article>
-                <article>Je suis une carte logement</article>
-                <article>Je suis une carte logement</article>
-                <article>Je suis une carte logement</article>
-                <article>Je suis une carte logement</article>
+                <article> <Link to="/lodgingdetail">Je suis une carte logement!</Link></article>
+                {logements.map((logement) => (
+                    <article key={logement.id}>{logement.title}</article>
+                ))
+                }
             </div>
         </>
 
