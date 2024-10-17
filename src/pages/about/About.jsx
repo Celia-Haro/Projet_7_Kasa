3d49573f7e1c466af66af6914a57046a4a410b74
Celia Haro <charo@keldin.fr>
2024-10-17 16:10:13 +0200

    feat: add about banner styles

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,12 @@
+import styles from '/src/pages/home/home.module.scss'
+import picture2 from '/src/assets/images/pictures/image2.png'
+
 function About() {
     return (
         <>
-            <h2> Qui sommes-nous </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptates culpa officiis,
-                magni adipisci voluptatum?</p>
+            <div className={styles.banner}>
+                <img src={picture2} alt="je suis une image" />
+            </div>
         </>
     )
 }
