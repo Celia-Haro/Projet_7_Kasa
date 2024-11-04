99f5783ac1c8606cb6962107e7c7b45492ade5e9
Celia Haro <haro.celia@hotmail.fr>
2024-11-04 13:31:29 +0100

    feat: header banner styles+responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,17 @@
+import styles from './home.module.scss'
+import picture1 from '/src/assets/images/image1.png'
+
+
 export default function Home() {
     return (
-        <h1>je suis la Home</h1>
+        <>
+            <div className={styles.banner}>
+                <img src={picture1} alt="je suis une image" />
+                <h1>
+                    <span>Chez vous, </span>
+                    <span>partout et ailleurs</span>
+                </h1>
+            </div>
+        </>
     )
 }
