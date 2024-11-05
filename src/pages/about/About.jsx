738b3311cb46745c8eb4784c5e664e9168070b79
Celia Haro <haro.celia@hotmail.fr>
2024-11-05 15:50:56 +0100

    fix: styles About + Collapse

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,13 +2,14 @@ import picture2 from '/src/assets/images/image2.png'
 import aboutList from '/src/assets/data/aboutList.js'
 import Collapse from '../../components/collapse/Collapse'
 import Banner from '../../components/banner/Banner'
+import styles from './about.module.scss'
 
 
 export default function About() {
     return (
         <>
             <Banner picture={picture2} />
-            <div>
+            <div className={styles.collapseContainer}>
                 {aboutList.map((item, index) => (
                     <Collapse key={index} title={item.title} content={item.content} />
                 ))
